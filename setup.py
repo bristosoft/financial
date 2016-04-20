8c7ee190b533fe36982e495e5d4b5938cd96824b
Kirk A. Jackson <jacksonkirka@bristosoft.com>
2016-04-19 21:28:19 -0400

    Create setup.py
    This is the setup.py file for pip installer.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'finance',
+  packages = ['finance'], # this must be the same as the name above
+  version = '1.0',
+  description = 'A Python module for basic finance and accounting.',
+  author = 'Kirk A. Jackson',
+  author_email = 'jacksonkirka@bristosoft.com',
+  url = 'https://github.com/bristosoft/finance', # use the URL to the github repo
+  download_url = 'https://github.com/bristosoft/finance/', # I'll explain this in a second
+  keywords = ['Finance', 'Accounting', 'Math'], # arbitrary keywords
+  classifiers = [],
+)
